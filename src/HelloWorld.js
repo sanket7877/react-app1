72ef783cde6b7c0dfb419af57d0438137e7e8818
sanket lakhatariya <sanket.lakhatariya@netxssolutions.com>
2021-04-15 14:49:30 +0530

    change HelloWorld.js file

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 class HelloWorld extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
     render() {
+
+        console.log("we are called");
         return (
             <div>
                 <h1>Welcome to Home page</h1>
