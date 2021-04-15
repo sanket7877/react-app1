bd995735da314859f6cf27fbc3e2c292f6d1c17b
sanket lakhatariya <sanket.lakhatariya@netxssolutions.com>
2021-04-15 14:41:14 +0530

    change HelloWorld.js file

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -5,7 +5,15 @@ class HelloWorld extends React.Component{
         return (
             <div>
                 <h1>Welcome to Home page</h1>
+                <div>
+                    <h2>Hey mr. How was your day?</h2>
+                </div>
+                <div>
+                    <p>Welcome to the universe world.</p>
+                </div>
             </div>
         );
     }
 }
+
+export default HelloWorld;
