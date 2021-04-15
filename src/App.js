cf1afd04a45d58c07a7c2e008c79d6ea5011340a
sanket lakhatariya <sanket.lakhatariya@netxssolutions.com>
2021-04-15 14:31:56 +0530

    Add new file

--- src/App.js
+++ src/App.js
@@ -1,51 +1,25 @@
-import React, {Component} from 'react';
+import logo from './logo.svg';
 import './App.css';
-import InstructorApp from "./component/InstructorApp";
-import RouteConfigExample from "./component/InstructorApp";
-import {Route, Switch, BrowserRouter as Router, Link} from "react-router-dom";
-import NotFound from "./component/NotFound";
-import NestingExample from "./component/InstructorApp";
-import Topics from "./component/Topics";
-import HOme from "./component/HOme";
 
-
-class   App extends React.Component{
-    render() {
-        return(
-            <Router>
-                <div style={{width: 1000, margin: '0 auto'}}>
-                    <ul>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/topics'>Topics</Link></li>
-                    </ul>
-
-                    <hr />
-                    <Switch>
-                    <Route exact path='/' > <HOme/></Route>
-                    <Route path='/topics' component={Topics} />
-                        <Route path="*">
-                            <NotFound/>
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
-// export default function   App() {
-//   return(
-//       <div className="container">
-//           <>
-//               <Switch>
-//                   <Route exact path="/" component={NestingExample}/>
-//                   <Route path="*"  component={NotFound}/>
-//               </Switch>
-//           </>
-//       </div>
-//   );
-//   }
-
-
-
---
+++ src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class HelloWorld extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Home page</h1>
+            </div>
+        );
+    }
+}
